a48ce5e7824b87c1e452c81d1a06ba505db519f4
StevieYu <yzm629@sina.cn>
2022-07-20 03:58:57 +0000

    feat: del alisa dir

--- .cracorc.js
+++ .cracorc.js
@@ -1,13 +1,8 @@
 const {resolve} = require('path')
-const {  } = require("@craco/craco");
+// const {  } = require("@craco/craco");
 
 module.exports = {
     webpack: {
-        alias:{
-            'amis$': resolve('src/alias/amis.js'),
-            'amis-ui$': resolve('src/alias/amis-ui.js'),
-            'amis-core$': resolve('src/alias/amis-core.js'),
-        },
         /** @typedef {import('webpack').Configuration} */
         configure: {
             resolve: {
