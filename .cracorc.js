a172c8c357b151183a6a7bf8b93879da6937c7dd
StevieYu <yzm629@sina.cn>
2022-07-20 04:33:50 +0000

    update

--- .cracorc.js
+++ .cracorc.js
@@ -1,10 +1,13 @@
 const {resolve} = require('path')
-// const {  } = require("@craco/craco");
+const { when } = require("@craco/craco");
 
 module.exports = {
     webpack: {
         /** @typedef {import('webpack').Configuration} */
         configure: {
+            output:{
+                publicPath: when(process.env.npm_lifecycle_event === 'gh', () => '/amis-editor-react-script-template/', '/'),
+            },
             resolve: {
                 conditionNames: ['require', 'node']
             }
